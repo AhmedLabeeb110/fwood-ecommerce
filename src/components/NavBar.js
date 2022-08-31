1ef97501d5c9054f9b82a95b5deab13b0e70963a
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-08-31 16:08:30 +0600

    jsx skeleton done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import Shop from "../pages/Shop";
+
+import { BiSearchAlt2, BiHeart, BiCart } from "react-icons/bi";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="nav">
+      <h2 className="logo">Miralou</h2>
+      <ul className="navlinks">
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="/shop">SHOP</Link>
+        </li>
+        <li>
+          <Link to="products">PRODUCTS</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <BiSearchAlt2 />
+        </li>
+        <li>
+          <BiHeart />
+        </li>
+        <li>
+          <BiCart />
+        </li>
+      </ul>
 
-export default NavBar
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default NavBar;
