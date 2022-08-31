de1c5722fd8d5a85bd6a009f2053dcfbcba3193a
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-08-31 18:06:18 +0600

    desktop version done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,40 @@
-import { Link, Routes, Route } from "react-router-dom";
+import { NavLink, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
 import Shop from "../pages/Shop";
 
 import { BiSearchAlt2, BiHeart, BiCart } from "react-icons/bi";
 
+import "./NavBar.css";
+
 const NavBar = () => {
   return (
-    <div className="nav">
-      <h2 className="logo">Miralou</h2>
-      <ul className="navlinks">
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/shop">SHOP</Link>
-        </li>
-        <li>
-          <Link to="products">PRODUCTS</Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <BiSearchAlt2 />
-        </li>
-        <li>
-          <BiHeart />
-        </li>
-        <li>
-          <BiCart />
-        </li>
-      </ul>
+    <div>
+      <div className="nav">
+        <h2 className="logo">Miralou</h2>
+        <ul className="navlinks">
+          <li>
+            <NavLink to="/">HOME</NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop">SHOP</NavLink>
+          </li>
+          <li>
+            <NavLink to="products">PRODUCTS</NavLink>
+          </li>
+        </ul>
+        <ul className="navicons">
+          <li>
+            <BiSearchAlt2 />
+          </li>
+          <li>
+            <BiHeart />
+          </li>
+          <li>
+            <BiCart />
+          </li>
+        </ul>
+      </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
