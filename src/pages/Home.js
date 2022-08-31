97212bc2c667f04577cc56309a494b72b677ddc6
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-08-31 18:39:05 +0600

    desktop header done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="heroSection">
+        <div className="heading">
+          The best solution for moisturizing the skin
+        </div>
+        <p className="hero-para">
+          Only included the most potential active waveo chocos
+        </p>
+        <div id="hero-btn">
+          <button>Buy Now</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
