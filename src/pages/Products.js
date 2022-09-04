80e99d88ca8c8a017d2ae9041eec964309e882f0
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-09-04 20:59:08 +0600

    card design started

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,27 @@
+import "./Products.css"
+
 import { useFetch } from "../hooks/useFetch";
 
 const Products = () => {
   // api with objects
   const { data } = useFetch(
-    "https://my-json-server.typicode.com/AhmedLabeeb110/fwood-ecommerce-db/allproducts"
+    "https://my-json-server.typicode.com/AhmedLabeeb110/fwood-ecommerce-db/allproducts "
   );
+  //          <img src={}  />
 
   return (
-    <div>{data && data.map((allproduct) => <h2>{allproduct.name}</h2>)}</div>
+    <div className="product">
+      {data &&
+        data.map((allproduct) => (
+          <div className="product-card" key={allproduct.id}>
+            <p className="sellingStatus">{allproduct.sellingStatus}</p>
+            <div className="productTitle">{allproduct.name}</div>
+            <div className="productPrice">{allproduct.price}</div>
+            <p className="productDescription">{allproduct.description}</p>
+            <p className="stock">{allproduct.stock}</p>
+          </div>
+        ))}
+    </div>
   );
 };
 
