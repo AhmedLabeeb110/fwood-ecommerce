2e08082af55205d28e0a2641157647af1cc987fe
Dorik (Labeeb) <ahmedlabeeb@Ahmeds-MacBook-Air.local>
2022-09-09 16:57:46 +0600

    context removed

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 
-import "./Product.css"
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+
+import "./Product.css";
 
 const Product = () => {
   const { id } = useParams();
   const url =
-    "https://my-json-server.typicode.com/AhmedLabeeb110/fwood-ecommerce-db/allproducts/" +
-    id;
+    "https://my-json-server.typicode.com/AhmedLabeeb110/fwood-ecommerce-db/allproducts/" + id;
   const { data: product } = useFetch(url);
 
   return (
@@ -19,11 +20,20 @@ const Product = () => {
           </div>
           <div className="product-Details">
             <span className="selling-Status">{product.sellingStatus}</span>
-            <p className="product-Stock">{product.stock ? "in stock" : "out of stock"}</p>
+            <p className="product-Stock">
+              {product.stock ? "in stock" : "out of stock"}
+            </p>
             <h3 className="product-Name">{product.name}</h3>
             <h3 className="product-Price">{product.price}</h3>
             <p className="product-Description">{product.description}</p>
-            {product.stock ? <button>Add to cart</button> : <button disabled>Add to cart</button>}
+            <AiFillPlusCircle />
+            <AiFillMinusCircle />
+            <br />
+            {product.stock ? (
+              <button>Add to cart</button>
+            ) : (
+              <button disabled>Add to cart</button>
+            )}
           </div>
         </div>
       )}
