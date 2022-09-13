df4660611535716d6c3f77ffa6cbc421c7ed7ac9
Dorik (Labeeb) <ahmedlabeeb@Ahmeds-MacBook-Air.local>
2022-09-13 20:52:57 +0600

    cart function  added

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -4,6 +4,7 @@ const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [items, setItems] = useState(0);
+  const [cartValue, setCartValue] = useState(null);
 
   const addToCartPlus = () => {
     setItems(items + 1);
@@ -14,8 +15,13 @@ export function CartProvider({ children }) {
       setItems(0);
     }
   };
+  const addToCart = () => {
+    setCartValue(items);
+  };
   return (
-    <CartContext.Provider value={{ items, addToCartPlus, addToCartMinus }}>
+    <CartContext.Provider
+      value={{ items, addToCartPlus, addToCartMinus, addToCart, cartValue }}
+    >
       {children}
     </CartContext.Provider>
   );
